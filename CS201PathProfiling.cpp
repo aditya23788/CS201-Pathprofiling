5242532eac7a3994cf70dce3cbf99faf99e0321b
zalex11 <zalex11@gmail.com>
2015-05-23 02:42:00 -0700

    working final

--- CS201PathProfiling.cpp
+++ CS201PathProfiling.cpp
@@ -20,6 +20,119 @@ using namespace llvm;
  
  
 namespace {
+class TopoSorter {
+public:
+  enum Color {WHITE, BLACK};
+  // Color marks per vertex (BB).
+  typedef DenseMap<const BasicBlock *, Color> BBColorMap;
+  BBColorMap ColorMap;
+  typedef std::vector<std::vector<const BasicBlock *>> BBVector;
+  BBVector SortedBBs;
+
+  void runToposort(std::vector<std::vector<const BasicBlock *>> &Loopset,std::vector<std::vector<const BasicBlock *>> &topsortBB) {
+	for(unsigned i = 0; i < Loopset.size();++i){
+		for (unsigned m = 0, n = Loopset[i].size(); m != n; ++m){
+			    errs() << "Loopset of "<<m<<" " << llvm::BlockAddress::get(const_cast<BasicBlock*>(Loopset[i][m])) << ":\n";
+
+    		  ColorMap[Loopset[i][m]] = TopoSorter::WHITE;		
+		}
+	    errs() << "Topological sort of " << Loopset[i][0] << ":\n";
+//change
+		DFSTopsort(Loopset[i][1],i,topsortBB,Loopset[i][0]);
+		      
+	}	
+  }
+  
+  std::vector<const BasicBlock *> Stack;  
+	void DFSTopsort(const BasicBlock* BB, int i, std::vector<std::vector<const BasicBlock *>> &topsortBB,const BasicBlock* end){
+		ColorMap[BB] = TopoSorter::BLACK;
+		errs() << "between\n";
+		topsortBB[i].push_back(BB);
+		//push bb pred on stack
+		errs() << "loop pred entry node\n";
+		for (const_pred_iterator PI = pred_begin(BB), E = pred_end(BB); PI != E; ++PI) {
+          const BasicBlock *P = *PI;
+		  errs() << llvm::BlockAddress::get(const_cast<BasicBlock*>(P))<<"\n";
+          Stack.push_back(P);
+        }
+		errs() << "before while\n";
+		while(!Stack.empty()){
+
+	        const BasicBlock *temp = Stack.back();
+			if(temp == end){
+                Stack.pop_back();
+                topsortBB[i].insert(topsortBB[i].begin(),temp);
+				break;
+            }
+			
+			bool push = true;
+			for(succ_const_iterator SI = succ_begin(temp), F = succ_end(temp); SI !=F;++SI){
+				const BasicBlock *s = *SI;
+				if(ColorMap[s] == TopoSorter::WHITE){
+				//bad
+					push = false;
+					break;
+				}
+			}
+			if(push){
+				ColorMap[temp] = TopoSorter::BLACK;
+				topsortBB[i].push_back(temp);
+				Stack.pop_back();
+				for (const_pred_iterator PI = pred_begin(temp), E = pred_end(temp); PI != E; ++PI) {
+          			const BasicBlock *P = *PI;
+		            Stack.push_back(P);
+					if(P == end){
+						Stack.pop_back();
+						topsortBB[i].insert(topsortBB[i].begin(),P);
+					}
+		        }
+			}
+			else{
+				Stack.pop_back();
+			}
+         }
+		
+        
+      }
+		
+
+  // Helper function to recursively run topological sort from a given BB.
+  // Returns true if the sort succeeded and false otherwise; topological sort
+  // may fail if, for example, the graph is not a DAG (detected a cycle).
+  bool recursiveDFSToposort(const BasicBlock *BB, unsigned i) {
+//    ColorMap[BB] = TopoSorter::GREY;
+    // For demonstration, using the lowest-level APIs here. A BB's successors
+    // are determined by looking at its terminator instruction.
+    const TerminatorInst *TInst = BB->getTerminator();
+	errs() << "before loop\n";
+	if(TInst->getNumSuccessors() > 1 && !isa<IndirectBrInst>(TInst))
+    for (unsigned I = 0, NSucc = TInst->getNumSuccessors(); I < NSucc; ++I) {
+		
+      BasicBlock *Succ = TInst->getSuccessor(I);
+//      Color SuccColor = ColorMap[Succ];
+		errs() << "\nblk: "<< llvm::BlockAddress::get(const_cast<BasicBlock*>(Succ))<<"\n";
+//      if (SuccColor == TopoSorter::WHITE) {
+//        if (!recursiveDFSToposort(Succ,i))
+//          return false;
+//      } else if (SuccColor == TopoSorter::GREY) {
+        // This detects a cycle because grey vertices are all ancestors of the
+        // currently explored vertex (in other words, they're "on the stack").
+//        outs() << " Detected cycle: edge from " << BB->getName() <<
+//                  " to " << Succ->getName() << "\n";
+//        return true;
+//      }
+//    }
+//	else
+//		return true;
+    // This BB is finished (fully explored), so we can add it to the vector.
+//    ColorMap[BB] = TopoSorter::BLACK;
+    SortedBBs[i].push_back(BB);
+	}
+    return true;
+  }
+};
+
+
 	// https://github.com/thomaslee/llvm-demo/blob/master/main.cc
 	static Function* printf_prototype(LLVMContext& ctx, Module *mod) {
 		std::vector<Type*> printf_arg_types;
@@ -33,7 +146,7 @@ namespace {
 		return func;
 	}
 
-	struct BasicBlocksDemo : public FunctionPass {
+	struct BasicBlocksDemo : public FunctionPass ,public TopoSorter{
 		static char ID;
 		LLVMContext *Context;
 		BasicBlocksDemo() : FunctionPass(ID) {}
@@ -54,7 +167,7 @@ namespace {
 
 
 		bool doInitialization(Module &M) {
-			errs() << "\n---------Starting BasicBlockDemo---------\n";
+			errs() << "\n---------Starting PathProfiling---------\n";
 			Context = &M.getContext();
 			bbCounter = new GlobalVariable(M, Type::getInt32Ty(*Context), false, GlobalValue::InternalLinkage, ConstantInt::get(Type::getInt32Ty(*Context), 0), "bbCounter");
 //			const char *finalPrintString = "BB Count: %d\n";
@@ -69,7 +182,7 @@ namespace {
 		 
 		//----------------------------------
 		bool doFinalization(Module &M) {
-		errs() << "-------Finished BasicBlocksDemo----------\n";
+		errs() << "-------Finished PathProfiling----------\n";
 		 
 		return false;
 		}
@@ -79,32 +192,61 @@ namespace {
 
 		errs() << "Function: " << F.getName() << '\n';
 		FindFunctionBackEdges(F,BackEdgesSet);
+
 		for (unsigned i = 0, e = BackEdgesSet.size(); i != e; ++i){
-			errs() << llvm::BlockAddress::get(const_cast<BasicBlock*>(BackEdgesSet[i].first)) << " to " << llvm::BlockAddress::get(const_cast<BasicBlock*>(BackEdgesSet[i].second))<<"\n";
+			errs() <<"Backedge: "<< llvm::BlockAddress::get(const_cast<BasicBlock*>(BackEdgesSet[i].first)) << " to " << llvm::BlockAddress::get(const_cast<BasicBlock*>(BackEdgesSet[i].second))<<"\n";
 		}
+
+
 		std::vector<std::vector<const BasicBlock*>>LoopSet(BackEdgesSet.size());
 		LoopConstruction(BackEdgesSet,LoopSet);	
 
-		for (unsigned i = 0, e = LoopSet.size(); i != e; ++i){
-		  for (unsigned m = 0, n = LoopSet[i].size(); m != n; ++m){
-			errs() << llvm::BlockAddress::get(const_cast<BasicBlock*>(LoopSet[i][m])) << "  ";
-		  }
-			errs() << "\n";
-		}
-		
-//		for(auto &BB: F) 
-		    //for(auto &I: BB)
-//			errs() << llvm::BlockAddress::get(const_cast<BasicBlock*>(&BB))<<"\n"<< BB << "\n";
+		for(auto &BB: F) 
+			errs() <<"BasicBlock: "<< llvm::BlockAddress::get(const_cast<BasicBlock*>(&BB))<<"\n"<< BB << "\n";
 
+		errs() << "Innermost Loops:\n";
+		for (unsigned i = 0, e = LoopSet.size(); i != e; ++i){
+			errs() <<"Loop"<<i<<" {";
+			errs() <<llvm::BlockAddress::get(const_cast<BasicBlock*>(LoopSet[i][0]));
+		  for (unsigned m = LoopSet[i].size()-1, n = 1; m >= n; --m){
+			errs() << ", "<<llvm::BlockAddress::get(const_cast<BasicBlock*>(LoopSet[i][m]));
+		  }
+			errs() << "}\n";
+		}
+/*
+		errs() << "unsort\n";
+		for(unsigned i = 0, e = LoopSet.size(); i != e; ++i){
+			for (unsigned m = 0, n = LoopSet[i].size(); m != n; ++m){
+            errs() << ", "<<llvm::BlockAddress::get(const_cast<BasicBlock*>(LoopSet[i][m]));
+          }
+            errs() << "}\n";
+        }
+*/
 
 		std::vector<int> retpaths;
 
 		if(!LoopSet.empty()){
 			Innermost_Loop(LoopSet);
-		
+			std::vector<std::vector<const BasicBlock *>> toposortloopset(LoopSet.size());
+		errs() << "runtoposort\n";
+		errs() << "size " << toposortloopset.size()<<"\n";	
+
+			runToposort(LoopSet,toposortloopset);
+
+		errs() << "sort?\n";		
+		errs() << "size " << toposortloopset.size();	
+        for(unsigned i = 0; i< toposortloopset.size(); ++i){
+			errs() << "firstloop \n";
+            for (unsigned m = 0, n = toposortloopset[i].size(); m != n; ++m){
+	            errs() << " , "<<llvm::BlockAddress::get(const_cast<BasicBlock*>(toposortloopset[i][m]));
+          } errs() << "\n";
+        }
+
+
+
 			std::map<std::pair<const BasicBlock*,const BasicBlock*>,int> edges;
-			computeEdgeWeights(LoopSet, edges, retpaths);
-			errs() << "num paths " << retpaths[0] << "\n";
+			computeEdgeWeights(toposortloopset, edges, retpaths);
+			//errs() << "num paths " << retpaths[0] << "\n";
 			Initcounter(LoopSet);
 			InsertBasicBlock(LoopSet, edges, F);
 
@@ -129,28 +271,29 @@ namespace {
 		// Add the footer to Main's BB containing the return 0; statement BEFORE calling runOnBasicBlock
 		if(isa<ReturnInst>(BB.getTerminator())) { // major hack?
 		addFinalPrintf(BB, Context, printf_func,LoopSet,retpaths);
+
+for(auto &BB: F)
+            errs() <<"BasicBlock: "<< llvm::BlockAddress::get(const_cast<BasicBlock*>(&BB))<<"\n"<< BB << "\n";
+
+
+
+    errs() << "after code:\n";
+        for (unsigned i = 0, e = LoopSet.size(); i != e; ++i){
+            errs() <<"Loop"<<i<<" {";
+            errs() <<llvm::BlockAddress::get(const_cast<BasicBlock*>(LoopSet[i][0]));
+          for (unsigned m = LoopSet[i].size()-1, n = 1; m >= n; --m){
+            errs() << ", "<<llvm::BlockAddress::get(const_cast<BasicBlock*>(LoopSet[i][m]));
+          }
+            errs() << "}\n";
+        }
+
+
 		}
 		//runOnBasicBlock(BB);
 		}
 
 		}
 
-		for(auto &BB: F)
-            //for(auto &I: BB)
-            errs() << llvm::BlockAddress::get(const_cast<BasicBlock*>(&BB))<< "\n";
-
-//Type::getInt32Ty(*Context), false, GlobalValue::InternalLinkage,
-//					ConstantArray::get(ArrayType::get(Type::getInt32Ty(*Context),totalpaths),llvm::ArrayRef<llvm::Constant*>() ));
-		
-			
-
-		for (unsigned i = 0, e = LoopSet.size(); i != e; ++i){
-			errs() << "Innermost Loop:";
-		  for (unsigned m = 0, n = LoopSet[i].size(); m != n; ++m){
-			errs() << llvm::BlockAddress::get(const_cast<BasicBlock*>(LoopSet[i][m])) << "  ";
-		  }
-			errs() << "\n";
-		}
 
 		 
 		return true; // since runOnBasicBlock has modified the program
@@ -161,12 +304,9 @@ namespace {
 		// For this, prepare the SCCGraph, and append to last BB?
 		void addFinalPrintf(BasicBlock& BB, LLVMContext *Context, /*GlobalVariable *var,*/ Function *printf_func,std::vector<std::vector<const BasicBlock *>> &LoopSet,std::vector<int> &retpaths) {
 		IRBuilder<> IRB(BB.getTerminator()); // Insert BEFORE the final statement
-		errs() << "beforeloop\n";
 		for(unsigned long i =0;i<LoopSet.size();i++){
-			errs() << "print";
 			std::stringstream s;
 			s << llvm::BlockAddress::get(const_cast<BasicBlock*>(LoopSet[i][0]));
-			errs() << "asdf "<< s.str()<<"\n";
 			//const char* finalstr = s.str().c_str();
 			std::string ss = "Path_";
 			ss.append(s.str());
@@ -176,11 +316,7 @@ namespace {
 			//loop over retpaths[i]
 			for(int j = 0; j<retpaths[i];j++){
 				std::string a = ss + std::to_string(j);
-//				ss.append(std::to_string(j));
-				errs() << a << "\n";
-//				finalstr = finalstr + '_'  + j;
 			 a =  a + ": %d\n";
-			errs() << a <<"\n";
 			const char *finalPrintString = a.c_str();
             Constant *format_const = ConstantDataArray::getString(*Context, finalPrintString);
 			BasicBlockPrintfFormatStr = new GlobalVariable(*((BB.getParent())->getParent()), llvm::ArrayType::get(llvm::IntegerType::get(*Context, 8), strlen(finalPrintString)+1), true, llvm::GlobalValue::PrivateLinkage, format_const, "BasicBlockPrintfFormatStr");
@@ -194,7 +330,6 @@ namespace {
 			    gepIndices[0] = initvalue;
 			    gepIndices[1] = idxValue;
 			    GetElementPtrInst* pcpointer = GetElementPtrInst::Create(pathctr,gepIndices,"pcptr",BB.getTerminator());
-				errs() << "loaded array1\n";
 	    		//load from array
 			    LoadInst* oldpc = new LoadInst(pcpointer,"oldpc",BB.getTerminator());
 
@@ -203,16 +338,12 @@ namespace {
         		indices.push_back(zero);
 		        indices.push_back(zero);
         		Constant *var_ref = ConstantExpr::getGetElementPtr(BasicBlockPrintfFormatStr, indices);
-				errs() << "loaded array2\n";
-		//		Value *bbc = IRB.CreateLoad(bbCounter);
 				CallInst *call = IRB.CreateCall2(printf_func, var_ref, oldpc);
-				errs() <<"set tail\n";
 	    	    call->setTailCall(false);
 
 			}
 
 		}
-		 
 		
 		}
 		};
@@ -263,11 +394,9 @@ void BasicBlocksDemo::LoopConstruction(SmallVectorImpl<std::pair<const BasicBloc
     std::vector<const BasicBlock *> Stack;
     for (unsigned i = 0, e = BackEdgesSet.size(); i != e; ++i){
           LoopSet[i].push_back(BackEdgesSet[i].second);
-	  //errs() << llvm::BlockAddress::get(const_cast<BasicBlock*>(LoopSet[i].back()))<<"\n";
 	  if(std::find(LoopSet[i].begin(),LoopSet[i].end(),BackEdgesSet[i].first)==LoopSet[i].end()){
 	      LoopSet[i].push_back(BackEdgesSet[i].first);
 	      Stack.push_back(BackEdgesSet[i].first);
-	      //errs() << llvm::BlockAddress::get(const_cast<BasicBlock*>(Stack.back()))<<"\n";
 	  }
 	  while(!Stack.empty()){
 	      
@@ -307,8 +436,10 @@ void BasicBlocksDemo::computeEdgeWeights(std::vector<std::vector<const BasicBloc
 		for(std::vector<std::vector<const BasicBlock*>>::iterator it = Loopset.begin();it!=Loopset.end();++it){
 			int count = 0;
 			const BasicBlock *start;
+			errs() << "Edge values: {";
 			for (std::vector<const BasicBlock*>::iterator iit = (*it).begin();iit!=(*it).end();++iit){
 				const BasicBlock * node = *iit;
+
 				if(count == 0){
 					//node is start
 					start = *iit;
@@ -318,12 +449,14 @@ void BasicBlocksDemo::computeEdgeWeights(std::vector<std::vector<const BasicBloc
 					numpaths[node] = 1;
 				}
 				else{
+
 					numpaths[node] =0;
 					for(succ_const_iterator SI = succ_begin(node), e = succ_end(node);SI!=e;++SI){
 						const BasicBlock * suc = *SI;
 						std::pair <const BasicBlock*,const BasicBlock*> edge (node,suc);
 						edges.insert(edges.begin(),{edge,numpaths[node]});
 						//printing
+						/*
 						errs() << "edge: ";
 						errs() << llvm::BlockAddress::get(const_cast<BasicBlock*> (node));
 						errs() << " to ";
@@ -331,6 +464,9 @@ void BasicBlocksDemo::computeEdgeWeights(std::vector<std::vector<const BasicBloc
 						errs() << " wegith ";
 						errs() << numpaths[node];
 						errs() << "\n";
+						*/
+						errs() << "(" << llvm::BlockAddress::get(const_cast<BasicBlock*> (node)) << "," << llvm::BlockAddress::get(const_cast<BasicBlock*> (suc)) 
+							<< ", "<<numpaths[node]<<"),";
 						//edges.insert(std::make_pair(iit,suc),numpaths[iit]);
 						numpaths[node] += numpaths[suc];
 					}
@@ -346,6 +482,7 @@ void BasicBlocksDemo::computeEdgeWeights(std::vector<std::vector<const BasicBloc
 					std::pair <const BasicBlock*,const BasicBlock*> edge (start,suc);
 					edges.insert(edges.begin(),{edge,numpaths[start]});
 					//printing
+					/*
 					errs() << "edge: ";
 					errs() << llvm::BlockAddress::get(const_cast<BasicBlock*> (start));
 					errs() << " to ";
@@ -353,6 +490,9 @@ void BasicBlocksDemo::computeEdgeWeights(std::vector<std::vector<const BasicBloc
 					errs() << " wegith ";
 					errs() << numpaths[start];
 					errs() << "\n";
+					*/
+					errs() << "("<< llvm::BlockAddress::get(const_cast<BasicBlock*> (start)) << "," << llvm::BlockAddress::get(const_cast<BasicBlock*> (suc))
+                    << ", "<< numpaths[start]<<"),";
 					numpaths[start] += numpaths[suc];
 					
 				}
@@ -361,6 +501,8 @@ void BasicBlocksDemo::computeEdgeWeights(std::vector<std::vector<const BasicBloc
 				}
 				
 			}
+			errs() << "}\n";
+
 			
 			
 			retpaths.push_back(numpaths[start]);
@@ -372,7 +514,7 @@ void BasicBlocksDemo::Initcounter(std::vector<std::vector<const BasicBlock*>> &L
 	IRBuilder<> IRB(const_cast<BasicBlock *>(Loopset[i][0])->getFirstInsertionPt());
 	Value *addAddr = IRB.CreateAdd(ConstantInt::get(Type::getInt32Ty(*Context), 0), ConstantInt::get(Type::getInt32Ty(*Context), 0));
 	IRB.CreateStore(addAddr, bbCounter);
-	errs()<< *(Loopset[i][0]) << "\n";
+	//errs()<< *(Loopset[i][0]) << "\n";
     }
 	
 }
@@ -381,9 +523,7 @@ void BasicBlocksDemo::InsertBasicBlock(std::vector<std::vector<const BasicBlock*
     
     for(auto &I : edges){
 	std::pair<const BasicBlock*,const BasicBlock*> BBpair = I.first;
-	errs() << llvm::BlockAddress::get(const_cast<BasicBlock*> (BBpair.first))<< " to " << llvm::BlockAddress::get(const_cast<BasicBlock*> (BBpair.second))<<"\n";
 	int edge = I.second;
-	errs() << "Before temp"<< "\n";
 	//const BasicBlock* temp = llvm::SplitEdge(const_cast<BasicBlock*>(BBpair.first), const_cast<BasicBlock*>(BBpair.second), this);
 	BasicBlock* temp = llvm::BasicBlock::Create(*Context,"",const_cast<BasicBlock *>(BBpair.second)->getParent() ,const_cast<BasicBlock *>(BBpair.second));
 	
@@ -403,7 +543,6 @@ void BasicBlocksDemo::InsertBasicBlock(std::vector<std::vector<const BasicBlock*
 	IRB.CreateStore(addAddr, bbCounter);
 	IRB.CreateBr(const_cast<BasicBlock *>(BBpair.second));
 	
-	errs() << *temp << "\n";
 	/*for(auto &BB: F) 
 	    errs() << llvm::BlockAddress::get(const_cast<BasicBlock*>(&BB))<<"\n"<< BB << "\n";*/
 	
@@ -434,11 +573,11 @@ void BasicBlocksDemo::updateArray(BasicBlock *bb, unsigned loopnum,int loopsize)
 	new StoreInst(newpc,pcpointer,bb->getTerminator());
 	}
 void BasicBlocksDemo::addtoEnd(std::vector<std::vector<const BasicBlock*>> &Loopset,std::vector<int> retpaths){
-	errs() <<"addtoend\n";
 	for (unsigned i = 0, e = Loopset.size(); i != e; ++i){
 		updateArray(const_cast<BasicBlock*>(Loopset[i][1]),i,retpaths[i]);
     }
 }
+
  
 char BasicBlocksDemo::ID = 0;
 static RegisterPass<BasicBlocksDemo> X("pathProfiling", "BasicBlocksDemo Pass", false, false);
